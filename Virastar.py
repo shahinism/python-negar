ec255e245ab512ae9374a28cb6ca40647909616c
Shahin Azad <ishahinism@gmail.com>
2012-08-07 23:52:53 +0430

    now evry thing should work except translating Arabic/English numbers to Persian

--- Virastar.py
+++ Virastar.py
@@ -12,12 +12,12 @@ class PersianEditor():
         """
         """
         self.text = text
-        self.fix_dashes = False
+        self.fix_dashes = True
         self.fix_three_dots = True
         self.fix_english_quotes = True
         self.fix_hamzeh = True
         self.cleanup_zwnj = False
-        self.fix_spacing_for_braces_and_quotes = False
+        self.fix_spacing_for_braces_and_quotes = True
         self.fix_arabic_numbers = False
         self.fix_english_numbers = False
         self.fix_misc_non_persian_chars = False
@@ -26,8 +26,8 @@ class PersianEditor():
         self.aggresive = True
         self.cleanup_kashidas = True
         self.cleanup_extra_marks = True
-        self.cleanup_spacing = False
-        self.cleanup_begin_and_end = False
+        self.cleanup_spacing = True
+        self.cleanup_begin_and_end = True
         self.cleanup()
     def cleanup(self):
         """
@@ -39,15 +39,15 @@ class PersianEditor():
 
         # replace double dash to ndash and triple dash to mdash
         if self.fix_dashes:
-            text = re.sub(r'-{3}', '—', text)
-            text = re.sub(r'-{2}', '–', text)
+            text = re.sub(ur'-{3}', ur'—', text)
+            text = re.sub(ur'-{2}', ur'–', text)
         # replace three dots with ellipsis
         if self.fix_three_dots:
-            text = re.sub(r'\s*\.{3,}', '…', text)
+            text = re.sub(ur'\s*\.{3,}', ur'…', text)
 
         # replace English quotes with their Persian equivalent
         if self.fix_english_quotes:
-            text = re.sub("([\"'`]+)(.+?)(\1)", '«\2»', text)
+            text = re.sub(ur"([\"'`]+)(.+?)(\1)", ur'«\2»', text)
 
         # should convert ه ی to ه
         # The original regex to find was: (\S)(ه[\s]+[ی])(\s)
@@ -108,29 +108,29 @@ class PersianEditor():
         # should fix outside and inside spacing for () [] {} "" «»
 
         if self.fix_spacing_for_braces_and_quotes:
-            text.sub(r'[   ‌]*(\()\s*([^)]+?)\s*?(\))[   ‌]*', ' 1\2\3 ')
-            text.sub(r'[   ‌]*(\[)\s*([^)]+?)\s*?(\])[   ‌]*', ' 1\2\3 ')
-            text.sub(r'[   ‌]*(\{)\s*([^)]+?)\s*?(\})[   ‌]*', ' 1\2\3 ')
-            text.sub(r'[   ‌]*(“)\s*([^)]+?)\s*?(”)[   ‌]*', ' 1\2\3 ')
-            text.sub(r'[   ‌]*(«)\s*([^)]+?)\s*?(»)[   ‌]*', ' 1\2\3 ')
+            text = re.sub(ur'[   ‌]*(\()\s*([^)]+?)\s*?(\))[   ‌]*', ur' \1\2\3 ', text)
+            text = re.sub(ur'[   ‌]*(\[)\s*([^)]+?)\s*?(\])[   ‌]*', ur' \1\2\3 ', text)
+            text = re.sub(ur'[   ‌]*(\{)\s*([^)]+?)\s*?(\})[   ‌]*', ur' \1\2\3 ', text)
+            text = re.sub(ur'[   ‌]*(“)\s*([^)]+?)\s*?(”)[   ‌]*', ur' \1\2\3 ', text)
+            text = re.sub(ur'[   ‌]*(«)\s*([^)]+?)\s*?(»)[   ‌]*', ur' \1\2\3 ', text)
 
         # : ; , ! ? and their persian equivalents should have one space after and no space before
         if self.fix_spacing_for_braces_and_quotes:
-            text.sub(r'[ ‌  ]*([:;,؛،.؟!]{1})[ ‌  ]*', '\1')
-            text.sub(r'([۰-۹]+):\s+([۰-۹]+)', '\1:\2')
+            text = re.sub(ur'[ ‌  ]*([:;,؛،.؟!]{1})[ ‌  ]*',ur'\1', text)
+            text = re.sub(ur'([۰-۹]+):\s+([۰-۹]+)', ur'\1:\2', text)
 
         # should fix inside spacing for () [] {} "" «»
         if self.fix_spacing_for_braces_and_quotes:
-            text.sub(r'(\()\s*([^)]+?)\s*?(\))', '\1\2\3')
-            text.sub(r'(\[)\s*([^)]+?)\s*?(\])', '\1\2\3')
-            text.sub(r'(\{)\s*([^)]+?)\s*?(\})', '\1\2\3')
-            text.sub(r'(“)\s*([^)]+?)\s*?(”)', '\1\2\3')
-            text.sub(r'(«)\s*([^)]+?)\s*?(»)', '\1\2\3')
+            text = re.sub(ur'(\()\s*([^)]+?)\s*?(\))', ur'\1\2\3', text)
+            text = re.sub(ur'(\[)\s*([^)]+?)\s*?(\])', ur'\1\2\3', text)
+            text = re.sub(ur'(\{)\s*([^)]+?)\s*?(\})', ur'\1\2\3', text)
+            text = re.sub(ur'(“)\s*([^)]+?)\s*?(”)', ur'\1\2\3', text)
+            text = re.sub(ur'(«)\s*([^)]+?)\s*?(»)', ur'\1\2\3', text)
 
         # should replace more than one space with just a single one
         if self.cleanup_spacing:
-            text.sub(r'[ ]+', ' ')
-            text.sub(r'([\n]+)[   ‌]', '\1')
+            text = re.sub(ur'[ ]+', ur' ', text)
+            text = re.sub(ur'([\n]+)[   ‌]', ur'\1', text)
 
         # remove spaces, tabs, and new lines from the beginning and end of file
         if self.cleanup_begin_and_end:
@@ -139,7 +139,7 @@ class PersianEditor():
         print text.encode('utf-8')
 
 if __name__ == "__main__":
-    sstring = unicode( 'همه ی شما ها می توانـــــید باشید ها عمه ات!!!؟؟؟', encoding='utf-8')
+    sstring = unicode( 'همه ی شما ها می توانـــــید باشید ها عمه ات!!!؟؟؟ (این هم برای تست پرانتز)', encoding='utf-8')
     run = PersianEditor(sstring)
     print run 
     
