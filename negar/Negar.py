fcb162555b6f5e9dcd099218212d83571d7ee0a0
Shahin Azad <ishahinism@gmail.com>
2012-08-22 10:52:49 +0430

    New structure V 0.2

---
+++ negar/Negar.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+import sys
+import getopt
+from   Virastar import PersianEditor
+
+# app data
+__author__ 	= "Shahin Azad 'Shahinism' (http://Shahinism.com)"
+__version__	= "0.2"
+__date__	= "Fri Aug 17 18:40:48 IRDT 2012"
+
+def helpMessage():
+    print """
+Welcome to Negar Persian editor program
+Usage: Negar [OPTIONS] -f [INPUT_FILE] -o [OUTPUT-FILE]
+
+Options:
+\t-h, --help                       Display this help and exit
+\t-V, --Version                    Print version number and exit
+\t-f, --file[=INPUT_FILE_NAME]     Specify [INPUT_FILE]. The file who you want to edit
+\t-o, --output[=OUTPUT_FILE_NAME]  Specify [OUT_PUT_FILE]. The file who you want the programs
+\t                                 output writes into it. If you don't specify this option
+\t                                 Negar will generate an auto file to save the result.
+Exit status:
+0   if OK,
+1   if unknown argumant passed to the Negar.
+
+To get more information visit the website: http://shahinism.github.com/Negar
+"""
+
+def main(argv):
+    output_file = "Negar_Output"
+    try:
+        opts, args = getopt.getopt(argv, "hVf:o:", ["help", "file", "output", "Version"])
+    except getopt.GetoptError:
+        helpMessage()
+        sys.exit(1)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            helpMessage()
+            sys.exit()
+        elif opt in ('-V', '--Version'):
+            print "You are using Negar version:"+__version__
+            sys.exit()
+        elif opt in ('-f', '--file'):
+            fileName = arg
+        elif opt in ('-o', '--output'):
+                output_file = arg
+    
+    try:
+        input_file  = open(fileName)
+        output_file = open(output_file, 'w') 
+        while True:
+            line = unicode(input_file.readline(), encoding='utf-8')
+            if len(line) == 0:
+                break
+            run_PE = PersianEditor(line)
+            output_file.write(run_PE.cleanup().encode('utf-8'))
+    finally:
+        input_file.close()
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ negar/Virastar.py
@@ -0,0 +1,277 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+
+class PersianEditor():
+    """
+    This class includes some functions to standard edit a Persian text
+    """
+    
+    def __init__(self, text):
+        """
+        This is the base part of the class
+        """
+        self.text = text
+        self.fix_dashes = True
+        self.fix_three_dots = True
+        self.fix_english_quotes = True
+        self.fix_hamzeh = True
+        self.hamzeh_with_yeh = True
+        self.cleanup_zwnj = False
+        self.fix_spacing_for_braces_and_quotes = True
+        self.fix_LTRM_RTLM = True
+        self.fix_arabic_numbers = True
+        self.fix_english_numbers = True
+        self.fix_misc_non_persian_chars = True
+        self.fix_perfix_spacing = True
+        self.fix_suffix_spacing = True
+        self.aggresive = True
+        self.cleanup_kashidas = True
+        self.cleanup_extra_marks = True
+        self.cleanup_spacing = True
+        self.cleanup()
+
+    def cleanup(self):
+        """
+        This is the main function who call other functions if need!
+        """
+        if self.fix_dashes:
+            self.fix_dashes_func()
+
+        if self.fix_three_dots:
+            self.fix_three_dots_func()
+
+        if self.fix_english_quotes:
+            self.fix_english_quotes_func()
+
+        if self.fix_hamzeh:
+            self.fix_hamzeh_func()
+
+        if self.cleanup_zwnj:
+            self.cleanup_zwnj_func()
+
+        if self.fix_misc_non_persian_chars:
+            self.char_validator()
+
+        if self.fix_arabic_numbers:
+            self.fix_arabic_numbers_func()
+
+        if self.fix_english_numbers:
+            self.fix_english_numbers_func()
+
+        if self.fix_perfix_spacing:
+            self.fix_perfix_spacing_func()
+            
+        if self.fix_suffix_spacing:
+            self.fix_suffix_spacing_func()
+
+        if self.aggresive:
+            self.aggresive_func()
+
+        if self.fix_spacing_for_braces_and_quotes:
+            self.fix_spacing_for_braces_and_quotes_func()
+
+        if self.fix_LTRM_RTLM:
+            self.fix_LTRM_RTLM_func()
+            
+        if self.cleanup_spacing:
+            self.cleanup_spacing_func()
+
+        return self.text
+        
+    def fix_dashes_func(self):
+        """
+        This function will replace double dash to ndash and triple dash to mdash
+        """
+        self.text = re.sub(ur'-{3}', ur'—', self.text)
+        self.text = re.sub(ur'-{2}', ur'–', self.text)
+
+    def fix_three_dots_func(self):
+        """
+        This function will replace three dots with ellipsis
+        """
+        self.text = re.sub(ur'\s*\.{3,}', ur'…', self.text)
+
+    def fix_english_quotes_func(self):
+        """
+        This function will replace English quotes with their persian equivalent
+        """
+        self.text = re.sub(ur"([\"'`]+)(.+?)(\1)", ur'«\2»', self.text)
+
+    def fix_hamzeh_func(self):
+        """
+        This function will replace end of any word which finished with 'ه ی' with
+        'هٔ' or 'ه‌ی'(if self.hamzeh_with_yeh == True)
+        """
+        if self.hamzeh_with_yeh:
+            self.text = re.sub(ur'(\S)(ه[\s]+[یي])(\s)',ur'\1ه‌ی\3', self.text)
+        else:
+            self.text = re.sub(ur'(\S)(ه[\s]+[یي])(\s)',ur'\1هٔ\3', self.text)
+
+    def cleanup_zwnj_func(self):
+        '''
+        This function will remove unnecessary zwnj that are succeeded/preceded by a space
+        '''
+        self.text = re.sub(ur'\s+|\s+', ur' ', self.text)
+
+    def char_validator(self):
+        """
+        This function will change invalid characters to validate ones.
+
+        it uses char_translator function to do it.
+        """
+        bad_chars  = u",;%يةك"
+        good_chars = u"،؛٪یهک"
+        self.text = self.char_translator(bad_chars, good_chars, self.text)
+
+    def fix_arabic_numbers_func(self):
+        """
+        This function will translate Arabic numbers to their Persian equivalants.
+
+        it uses char_translator function to do it.
+        """
+        persian_numbers = u"۱۲۳۴۵۶۷۸۹۰"
+        arabic_numbers = u"١٢٣٤٥٦٧٨٩٠"
+        self.text = self.char_translator(arabic_numbers, persian_numbers, self.text)
+
+    def fix_english_numbers_func(self):
+        """
+        This function will translate English numbers to their Persian equivalants.
+
+        it will avoid to do this translation at a English string!
+        it uses char_translator function to do it.
+        """
+        persian_numbers = u"۱۲۳۴۵۶۷۸۹۰"
+        english_numbers = u"1234567890"
+        self.text = self.char_translator(english_numbers, persian_numbers, self.text)
+
+        #Followilng commands will help Negar to avoid chang english numbers in strings
+        #like 'Text12', 'Text_12' & other string like this
+        self.text = re.sub(ur'[a-z\-_]{2,}[۰-۹]+|[۰-۹]+[a-z\-_]{2,}',
+                           lambda m:
+                           self.char_translator(persian_numbers, english_numbers,  m.group()),
+                           self.text)
+            
+            
+    def fix_perfix_spacing_func(self):
+        """
+        Put zwnj between word and prefix (mi* nemi*)
+
+        there's a possible bug here: می and نمی could separate nouns and not prefix
+        """
+        self.text = re.sub(ur"\s+(ن?می)\s+",ur' \1‌', self.text)
+
+    def fix_suffix_spacing_func(self):
+        """
+        put zwnj between word and suffix (*ha[ye] *tar[in])
+        """
+        # If you cant read it: '\s+ha(ye)?|(tar(ye)?(noon)?)\s+'
+        self.text = re.sub(ur'\s+(تر(ی(ن)?)?|ها(ی)?)\s+', ur'‌\1 ', self.text) 
+
+        list = [ur'بهتر',ur'اشتها']
+        regex = re.compile(ur'(\S)+(ها)', re.U)
+        find_regex = regex.search(self.text)
+        if find_regex:
+            if not find_regex.group() in list: # if word is not in list :D
+                self.text = regex.sub(ur'\1‌\2 ', self.text)
+        
+    def aggresive_func(self):
+        """
+        Aggressive Editing
+        """
+        # replace more than one ! or ? mark with just one
+        if self.cleanup_extra_marks:
+            self.text = re.sub(ur'(!){2,}', ur'\1', self.text)
+            self.text = re.sub(ur'(؟){2,}', ur'\1', self.text)
+            
+        # should remove all kashida
+        if self.cleanup_kashidas:
+            self.text = re.sub(ur'ـ+', "", self.text)
+        
+    def fix_spacing_for_braces_and_quotes_func(self):
+        """
+        This function will fix the braces and quotes spacing problems.
+        """
+        # ()[]{}""«» should have one space before and one virtual space after (inside)
+        self.text = re.sub(ur'[ ‌]*(\()\s*([^)]+?)\s*?(\))[ ‌]*', ur' \1‌\2‌\3 ', self.text)
+        self.text = re.sub(ur'[ ‌]*(\[)\s*([^)]+?)\s*?(\])[ ‌]*', ur' \1‌\2‌\3 ', self.text)
+        self.text = re.sub(ur'[ ‌]*(\{)\s*([^)]+?)\s*?(\})[ ‌]*', ur' \1‌\2‌\3 ', self.text)
+        self.text = re.sub(ur'[ ‌]*(“)\s*([^)]+?)\s*?(”)[ ‌]*', ur' \1‌\2‌\3 ', self.text)
+        self.text = re.sub(ur'[ ‌]*(«)\s*([^)]+?)\s*?(»)[ ‌]*', ur' \1‌\2‌\3 ', self.text)
+        # : ; , ! ? and their persian equivalents should have one space after and no space before
+        self.text = re.sub(ur'[ ‌ ]*([:;,؛،.؟!]{1})[ ‌ ]*',ur'‌\1 ', self.text)
+        self.text = re.sub(ur'([۰-۹]+):\s+([۰-۹]+)', ur'\1:\2', self.text)
+        # should fix inside spacing for () [] {} "" «»
+        self.text = re.sub(ur'(\()\s*([^)]+?)\s*?(\))', ur'\1\2\3', self.text)
+        self.text = re.sub(ur'(\[)\s*([^)]+?)\s*?(\])', ur'\1\2\3', self.text)
+        self.text = re.sub(ur'(\{)\s*([^)]+?)\s*?(\})', ur'\1\2\3', self.text)
+        self.text = re.sub(ur'(“)\s*([^)]+?)\s*?(”)', ur'\1\2\3', self.text)
+        self.text = re.sub(ur'(«)\s*([^)]+?)\s*?(»)', ur'\1\2\3', self.text)
+
+    def fix_LTRM_RTLM_func(self):
+        """
+        This function will fix 'ltr mark/rtl mark' problem in the text
+        """
+        test = re.search(ur'([\(\)\)\(])[a-z]', self.text)
+        if test:
+            print 'yes'
+        self.text = re.sub(ur'(\()([a-z])(\))', ur'\1‎\2\3‎', self.text)
+        
+    def cleanup_spacing_func(self):
+        self.text = re.sub(ur'[ ]+', ur' ', self.text)
+        self.text = re.sub(ur'([\n]+)[ ‌]', ur'\1', self.text)
+
+
+    def char_translator(self, fromchar, tochar, whichstring):
+        """
+        This function will translate the 'whichstring' character by character from
+
+        'fromchar' to 'tochar'. My old function is writed after this. in this new
+        function I can return the newstring, but I can't check the length of fromchar
+        and tochar! Why? I don't know!
+        """
+        newstring = whichstring
+        for i in range(len(fromchar)):
+            newstring = re.sub(fromchar[i], tochar[i], newstring)
+        return newstring
+#    def char_translator(self, fromchar, tochar):
+#        if len(fromchar) == len(tochar):
+#            for i in range(len(fromchar)):
+#                self.text = re.sub(fromchar[i], tochar[i], self.text)
+#        else:
+#            raise "in function char_translator fromchar and to char doesn't have same length'"
+        
+        
+def helpMessage():
+    print """Hi, I'm negars virastar!
+you can use me with a command like this:
+./Virastar.py [FILE-NAME]/[Argumant]
+My Argumants are:
+\t\t--help : show this message
+\n\n\n
+you can use me more effectively with a command like this:
+\t\t$./Virastar.py FILE-NAME > OutPut\n"""
+        
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        helpMessage()
+        sys.exit()
+    if sys.argv[1].startswith('--'):
+        option = sys.argv[1][2:]
+        if option == 'help':
+            helpMessage()
+    else:
+        try:
+            fileName = sys.argv[1]
+            file = open(fileName)
+            while True:
+                line = unicode( file.readline(), encoding='utf-8')
+                if len(line) == 0:
+                    break
+                    #print line
+                run2 = PersianEditor(line)
+                print run2.cleanup().encode('utf-8'),
+        finally:
+            file.close()
