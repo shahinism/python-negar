1f2559715d26ea1ef6be5bf99e9648217327b277
Shahin Azad <ishahinism@gmail.com>
2013-02-09 15:47:00 +0330

    Some modification in datas

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
+# Virastar is a python module to check and edit Persian text as good
+# as possible. It'll edit punctuation and standard spacing on your
+# Persian text.
+
+# To install python-negar module you can read instructor in the Readme
+# file. or just can run python setup.py install ;-)
+
+# Copyright (C) <2013> <Shahin Azad [ishahinism at Gmail]>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
