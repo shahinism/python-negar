e0a9642c3ee5e90d0d22877b260f32da9c518bad
Shahin Azad <ishahinism@gmail.com>
2013-02-09 15:35:01 +0330

    Some clean up on python-negar module

--- negar/virastar.py
+++ negar/virastar.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Virastar is a python module to check and edit Persian text as good
+# as possible. It'll edit punctuation and standard spacing on your
+# Persian text.
+# Copyright (C) <2013> <Shahin Azad [ishahinism at Gmail]>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import os
 import re
 import codecs
