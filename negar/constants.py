0c137a2a256a754543c03aebdabaa175ae306941
javad <javadr@gmail.com>
2022-04-07 08:13:39 +0430

    Fixed another issue with English digits and minus sign

--- negar/constants.py
+++ negar/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 LOGO = (Path(__file__).parent.absolute()/"logo.png").as_posix()
 DATAFILE = Path(__file__).parent.absolute()/"data/untouchable.dat"
