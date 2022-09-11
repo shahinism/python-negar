f09c3063dc66053a95c60ab626375c9da3183a62
javad <javadr@gmail.com>
2022-09-11 17:07:28 +0430

    (fix) issue with some words

--- negar/constants.py
+++ negar/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-__version__ = "1.1.15"
+__version__ = "1.1.16"
 
 DATAFILE = Path(__file__).parent.absolute()/"data/untouchable.dat"
 USERFILE = Path.home()/".python-negar"
