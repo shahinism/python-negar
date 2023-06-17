e1270baff7c7964e6067d6c85867e02464251d5a
javad <javadr@gmail.com>
2023-06-18 03:23:03 +0330

    (style) _assertEqual decorator

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -1,24 +1,33 @@
 import unittest
+import functools
 from negar.virastar import PersianEditor
 
 
 class Test(unittest.TestCase):
+    def _assertEqual(message=""):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(self, *args, **kwargs):
+                func(self, *args, **kwargs)
+                return self.assertEqual(f"{PersianEditor(self.input_)}", self.output_, message)
+            return wrapper
+        return decorator
+
     # @unittest.skip("")
+    @_assertEqual('trim leading trailing whitespaces')
     def test_trim_leading_trailing_whitespaces(self):
-        input_ = "فضا های         خالی     بیش          از       حد"
-        output_ = "فضاهای خالی بیش از حد"
-        self.assertEqual(f"{PersianEditor(input_)}", output_, 'trim leading trailing whitespaces')
+        self.input_ = "فضا های         خالی     بیش          از       حد"
+        self.output_ = "فضاهای خالی بیش از حد"
 
+    @_assertEqual('fix hamzeh')
     def test_fix_hamzeh(self):
-        input_ = "کلماتی که با 'ی' پسوند همراه هستند مانند 'همه ی ' -- و البته امکان جایگزینی آن با حمزه 'ء' ( در صورت انتخاب کاربر )"
-        output_ = "کلماتی که با «ی» پسوند همراه هستند مانند «همه‌ی» – و البته امکان جایگزینی آن با حمزه «ء» (در صورت انتخاب کاربر)"
-        self.assertEqual(f"{PersianEditor(input_)}", output_, 'fix hamzeh')
+        self.input_ = "کلماتی که با 'ی' پسوند همراه هستند مانند 'همه ی ' -- و البته امکان جایگزینی آن با حمزه 'ء' ( در صورت انتخاب کاربر )"
+        self.output_ = "کلماتی که با «ی» پسوند همراه هستند مانند «همه‌ی» – و البته امکان جایگزینی آن با حمزه «ء» (در صورت انتخاب کاربر)"
 
+    @_assertEqual('fix parantheses')
     def test_fix_parantheses(self):
-        input_ = "پرانتز ها  یا دیگر علایم ؛ ( نگار )"
-        output_ = "پرانتزها یا دیگر علایم؛ (نگار)"
-        self.assertEqual(f"{PersianEditor(input_)}", output_, 'fix parantheses')
-
+        self.input_ = "پرانتز ها  یا دیگر علایم ؛ ( نگار )"
+        self.output_ = "پرانتزها یا دیگر علایم؛ (نگار)"
 
 
 if __name__ == '__main__':
